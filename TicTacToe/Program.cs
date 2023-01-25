a146f718a585df29c1f59bd4633d8f958a699cc4
rcroft1 <rcroft1@byu.edu>
2023-01-25 12:36:47 -0700

    first push... needs to find winner

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,63 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to tic tac toe!");
+
+            bool winner = false;
+            // List<int> moveCoordinates = new System.Collections.Generic.List<int>() { 0, 0 };
+            // int[] moveCoordinates = new int[2];
+            string[,] board = new string[3, 3];
+            int k = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    board[i, j] = "*";
+                }
+            }
+
+            while (winner == false)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Input the X coordinate of your move ranging from 0 to 2: ");
+                int X = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input the Y coordinate of your move ranging from 0 to 2: ");
+                int Y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+
+
+                if (k % 2 == 0)
+                {
+                    board[Y, X] = "X";
+                }
+                else
+                {
+                    board[Y, X] = "O";
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write(board[i, j]);
+                    }
+                    Console.WriteLine("\n");
+                }
+
+                k++;
+            }
+
+            if (k % 2 == 1)
+            {
+                Console.WriteLine("Player X has won the game!");
+            }
+            else
+            {
+                Console.WriteLine("Player O has won the game!");
+            }
+            
+            
         }
     }
 }
